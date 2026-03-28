359cc893d85fd397155a61f26e6cdbf9fe01bad9
Pvolvxd <karla.choque@tecsup.edu.pe>
2026-03-28 16:06:25 -0500

    agrego setters and toString()

--- src/main/java/pe/edu/tecsup/lab/StudentModel.java
+++ src/main/java/pe/edu/tecsup/lab/StudentModel.java
@@ -23,4 +23,17 @@ public class StudentModel {
     public String getApellido() {
         return apellido;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Student[" + id + "] " + nombre + " " + apellido;
+    }
 }
