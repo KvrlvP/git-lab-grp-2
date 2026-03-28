181c23d1b2771dbe3269814a83f1688bf4c3fc8c
Karla Choque <paolaz2005d@gmail.com>
2026-03-28 15:57:35 -0500

    add logic to getStudents()

--- src/main/java/pe/edu/tecsup/lab/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab/StudentController.java
@@ -1,9 +1,14 @@
 package
 pe.edu.tecsup.lab;
 import
-java.util.List;
+java.util.*;
 public class
 StudentController
 {
-  
+  public List<Object> getStudents() {
+    List<Object> students = new ArrayList<>();
+    students.add("Ana Garcia");
+    students.add("Luis Perez");
+    return students;
+  }
 }
