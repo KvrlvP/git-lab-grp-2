5b90e9312ca9ec6c15d7fde2afb5a3370d997077
Pvolvxd <karla.choque@tecsup.edu.pe>
2026-03-28 16:05:34 -0500

    agrego constructor and getters

--- src/main/java/pe/edu/tecsup/lab/StudentModel.java
+++ src/main/java/pe/edu/tecsup/lab/StudentModel.java
@@ -1,9 +1,26 @@
-package
-pe.edu.tecsup.lab;
-public class
-StudentModel
-{
-  private int id;
-  private String nombre;
-  private String apellido;
+package pe.edu.tecsup.lab;
+
+public class StudentModel {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+
+    public StudentModel(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
 }
