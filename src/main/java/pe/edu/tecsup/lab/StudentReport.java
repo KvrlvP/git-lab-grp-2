82a868ed9b4bc17da5d472d25f939ef423d9c613
Lolcmz <lolocamz148@gmail.com>
2026-03-28 16:10:14 -0500

    implemento generateReport()

--- src/main/java/pe/edu/tecsup/lab/StudentReport.java
+++ src/main/java/pe/edu/tecsup/lab/StudentReport.java
@@ -1,7 +1,16 @@
-package
-pe.edu.tecsup.lab;
-public class
-StudentReport
-{
+package pe.edu.tecsup.lab;
 
+import java.util.List;
+
+public class StudentReport {
+
+    // Clase para reportes de estudiantes
+
+    public void generateReport(List<StudentModel> students) {
+        System.out.println("=== REPORTE ===");
+        for (StudentModel s : students) {
+            System.out.println(s.toString());
+        }
+        System.out.println("Total: " + students.size());
+    }
 }
